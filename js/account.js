381e6772e708d5dabd69066d9def214f3de03e01
DeddyChandra <6181801007@student.unpar.ac.id>
2021-01-12 14:52:58 +0700

    login dan register done

---
+++ js/account.js
@@ -0,0 +1,11 @@
+let facebook = document.getElementById("facebook");
+let google = document.getElementById("google");
+
+facebook.onclick = function(){
+    window.open("http://www.facebook.com");
+};
+
+
+google.onclick = function(){
+    window.open("http://www.google.com");
+};
